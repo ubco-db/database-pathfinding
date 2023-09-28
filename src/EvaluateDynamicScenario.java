45791f3b82e6fe624100077bf1bc5c933821b845
te-sa <teresa_saller@freenet.de>
2023-09-28 09:02:20 -0700

    Added Walls from other repo, made test file

---
+++ src/EvaluateDynamicScenario.java
@@ -0,0 +1,9 @@
+public class EvaluateDynamicScenario {
+    public static void main(String[] args) {
+        // IDEA: copy over DBA* code from EvaluateScenario
+        // perform DBA*
+        // build wall
+        // recompute database
+        // compare databases
+    }
+}
