d27f7c36b09ef5a621453cef9deaa3a515fb39ec
te-sa <teresa_saller@freenet.de>
2023-10-09 10:00:40 -0700

    Added more code for path comparison - running into issue for certain goalIds

--- src/VisualizeDBChanges.java
+++ src/VisualizeDBChanges.java
@@ -26,41 +26,66 @@ public class VisualizeDBChanges {
 
         // fix start, colour it (yellow)
         int startId = 13411;
-        int goalId;
         ArrayList<Integer> goalIds = new ArrayList<>();
 
         // iterate over all goals (open spots no wall)
         for (int i = 16; i < map.rows; i++) {
             for (int j = 0; j < map.cols; j++) {
-                goalId = map.getId(i, j);
-                if (!map.isWall(i, j) && (goalId != startId)) {
-                    goalIds.add(goalId);
+                if (!map.isWall(i, j)) {
+                    goalIds.add(map.getId(i, j));
                 }
             }
         }
 
-        System.out.println(goalIds.contains(startId));
+        // remove startId from list of goals
+        goalIds.remove((Integer) startId);
+
+        // print number of goals (6175)
+        System.out.println("Number of goals: " + goalIds.size());
 
         // compute paths to all goals, store in HashMap of arrays (goal state as key)
-//        HashMap<Integer,ArrayList<SearchState>> paths = new HashMap<>();
-//        for (int goalId: goalIds) {
-//            paths.put(goalId, getDBAStarPath(map, startId, goalId));
-//        }
+        HashMap<Integer,ArrayList<SearchState>> paths = new HashMap<>();
+        for (int goalId: goalIds) {
+            System.out.println("Goal Id:" + goalId); // 11922 throws an index out of bounds
+            paths.put(goalId, getDBAStarPath(map, startId, goalId));
+        }
 
         // add wall
         ArrayList<SearchState> wallLocation = new ArrayList<>();
         Walls.addWall(PATH_TO_MAP, wallLocation, map);
-        wallLocation.add(new SearchState(13558));
+        int wallId = 13558;
+        wallLocation.add(new SearchState(wallId));
         map = new GameMap(PATH_TO_MAP);
 
-        // iterate over all goals (open spots no wall, remove the spot where a wall was added)
+        // remove wallId from list of goals
+        goalIds.remove((Integer) wallId);
 
+        // iterate over all goals (open spots no wall, remove the spot where a wall was added)
         // compare each path with stored path to same location, if identical, do nothing, if not, colour it (purple)
+        ArrayList<SearchState> newPath;
+        ArrayList<SearchState> oldPath;
+        for (int goalId: goalIds) {
+            newPath = getDBAStarPath(map, startId, goalId);
+            oldPath = paths.get(goalId);
+            // compare each path with stored path to the same location
+            boolean pathEqual = isPathEqual(newPath, oldPath);
+        }
 
         // remove wall
         Walls.removeWall(PATH_TO_MAP, wallLocation, map);
         long timeTaken = System.currentTimeMillis() - startTime;
         System.out.println("This run took: " + timeTaken);
+    }
+
+    private static boolean isPathEqual(ArrayList<SearchState> newPath, ArrayList<SearchState> oldPath) {
+        // if path length differs, they are not equal
+        if (newPath.size() != oldPath.size()) return false;
+
+        for (int i = 0; i < newPath.size(); i++) {
+            // comparing SearchStates (have an equals-method)
+            if (!newPath.get(i).equals(oldPath.get(i))) return false;
+        }
+        return true;
     }
 
     private static String getImageName(String wallStatus) {
