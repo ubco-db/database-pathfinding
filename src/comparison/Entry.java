ecea9a5ab9e92255952b078c94e6824d4b69b4ef
te-sa <teresa_saller@freenet.de>
2023-10-17 16:47:50 -0700

    Fixed NaN values being returned

--- src/comparison/Entry.java
+++ src/comparison/Entry.java
@@ -11,10 +11,14 @@ public class Entry implements Comparable<Entry> {
         this.percentageChanged = percentageChanged;
         this.wallId = wallId;
         double[] amountChanged = getAmountChanged(changedPaths);
-        this.output = String.format("Wall at: %d. Percentage of goals changed: %.2f.\t%.2f percent of goals had a 0-25%% change in their paths, %.2f percent had a 25-50%% change, %.2f percent had a 50-75%% change, and %.2f percent had a 75-100%% change", wallId, percentageChanged, amountChanged[0], amountChanged[1], amountChanged[2], amountChanged[3]);
+        String percentChangeOfPathToGoal = String.format("%.2f percent of goals had a 0-25%% change in their paths, %.2f percent had a 25-50%% change, %.2f percent had a 50-75%% change, and %.2f percent had a 75-100%% change", amountChanged[0], amountChanged[1], amountChanged[2], amountChanged[3]);
+        this.output = String.format("Wall at: %d. Percentage of goals changed: %.2f.", wallId, percentageChanged) + "\t" + percentChangeOfPathToGoal;
     }
 
     private double[] getAmountChanged(ArrayList<ChangedPath> changedPaths) {
+        int n = changedPaths.size();
+        if (n == 0) return new double[]{0, 0, 0, 0};
+
         double[] quartiles = new double[4];
         for (ChangedPath changedPath : changedPaths) {
             double p = changedPath.getPercentageOfPathToGoalChanged();
@@ -23,8 +27,6 @@ public class Entry implements Comparable<Entry> {
             else if (p < 75) quartiles[2]++;
             else quartiles[3]++;
         }
-        int n = changedPaths.size();
-        // TODO: fix NaN values being returned
         return new double[]{quartiles[0] / n * 100, quartiles[1] / n * 100, quartiles[2] / n * 100, quartiles[3] / n * 100};
     }
 
