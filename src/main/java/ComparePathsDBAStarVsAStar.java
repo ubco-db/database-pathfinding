5385a3ec05c7e8fc0d5c0ac76d146ae05433515e
te-sa <teresa_saller@freenet.de>
2024-04-02 11:24:28 -0700

    Refactored to avoid logging so much

--- src/main/java/ComparePathsDBAStarVsAStar.java
+++ src/main/java/ComparePathsDBAStarVsAStar.java
@@ -43,13 +43,17 @@ public class ComparePathsDBAStarVsAStar {
         // Map is used to generate images
         GameMap map = dbaStar.getMap();
 
-        double maxSuboptimality = -1;
-        BigDecimal averageSuboptimality = new BigDecimal(0);
-
-        int goalIdWithMaxSuboptimality = -1;
-        int startIdWithMaxSuboptimality = -1;
+        double averageSuboptimalityGlobal = 0;
+        double maxSuboptimalityGlobal = -1;
+        double goalIdWithMaxSuboptimalityGlobal = -1;
+        double startIdWithMaxSuboptimalityGlobal = -1;
 
         for (int goalId : goalIds) {
+            double totalSuboptimalityLocal = 0;
+            double maxSuboptimalityLocal = -1;
+            int goalIdWithMaxSuboptimalityLocal = -1;
+            int startIdWithMaxSuboptimalityLocal = -1;
+
             for (int startId: goalIds) {
 //            dbaStarUtil.recomputeWallAdditionNoLogging(goalId, dbaStar);
 //            dbaStarUtil.recomputeWallRemovalNoLogging(goalId, dbaStar);
@@ -61,16 +65,14 @@ public class ComparePathsDBAStarVsAStar {
 
                 StatsRecord aStarStats = new StatsRecord();
                 ArrayList<SearchState> optimalPath = aStar.computePath(new SearchState(startId), new SearchState(goalId), aStarStats);
-                logger.info("AStar path cost: " + aStarStats.getPathCost() + " DBAStar path cost: " + dbaStats.getPathCost());
 
                 double subOptimality = ((((double) dbaStats.getPathCost()) / aStarStats.getPathCost()) - 1) * 100.0;
-                logger.info("Suboptimality: " + subOptimality);
-                averageSuboptimality = averageSuboptimality.add(BigDecimal.valueOf(subOptimality));
+                totalSuboptimalityLocal += subOptimality;
 
-                if (maxSuboptimality < subOptimality) {
-                    maxSuboptimality = subOptimality;
-                    goalIdWithMaxSuboptimality = goalId;
-                    startIdWithMaxSuboptimality = startId;
+                if (maxSuboptimalityLocal < subOptimality) {
+                    maxSuboptimalityLocal = subOptimality;
+                    goalIdWithMaxSuboptimalityLocal = goalId;
+                    startIdWithMaxSuboptimalityLocal = startId;
                 }
 
                 if (path == null || path.isEmpty()) {
@@ -79,9 +81,21 @@ public class ComparePathsDBAStarVsAStar {
                     map.computeCentroidMap().outputImage(DBA_STAR_DB_PATH + "optimal_path_" + startId + "_" + goalId + ".png", optimalPath, dbaStar.getSubgoals());
                 }
             }
+
+            double averageSuboptimalityLocal = totalSuboptimalityLocal / (goalIds.size());
+            logger.info("Average suboptimality: " + averageSuboptimalityLocal);
+            logger.info("Max. suboptimality: " + maxSuboptimalityLocal + " for path from " + startIdWithMaxSuboptimalityLocal + " to " + goalIdWithMaxSuboptimalityLocal);
+
+            if (maxSuboptimalityGlobal < maxSuboptimalityLocal) {
+                maxSuboptimalityGlobal = maxSuboptimalityLocal;
+                goalIdWithMaxSuboptimalityGlobal= goalIdWithMaxSuboptimalityLocal;
+                startIdWithMaxSuboptimalityGlobal = startIdWithMaxSuboptimalityLocal;
+            }
+
+            averageSuboptimalityGlobal += averageSuboptimalityLocal;
         }
 
-        logger.info("Average suboptimality: " + averageSuboptimality.divide(BigDecimal.valueOf(goalIds.size()),5, RoundingMode.HALF_UP));
-        logger.info("Max. suboptimality: " + maxSuboptimality + " for path from " + startIdWithMaxSuboptimality + " to " + goalIdWithMaxSuboptimality);
+        logger.info("Average suboptimality whole map: " + averageSuboptimalityGlobal / (goalIds.size()));
+        logger.info("Max. suboptimality: " + maxSuboptimalityGlobal + " for path from " + startIdWithMaxSuboptimalityGlobal + " to " + goalIdWithMaxSuboptimalityGlobal);
     }
 }
